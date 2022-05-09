018cc5df5d265562461b4b01bfa323c6f3bc03c5
csmoran <98026111+csmoran@users.noreply.github.com>
2022-05-09 17:49:01 +0100

    Update prey.py

--- client-prey/src/prey.py
+++ client-prey/src/prey.py
@@ -1,10 +1,8 @@
-import numpy as np
-
 class Prey():
 
     def __init__(self, x, y, z, dx, dy, dz, width, height):
-        self.position = np.array([x, y, z])
-        self.velocity = np.array([dx,dy,dz])
+        self.position = [x, y, z]
+        self.velocity = [dx,dy,dz]
         self.acceleration = 0
         self.max_force = 0.3
         self.max_speed = 5
